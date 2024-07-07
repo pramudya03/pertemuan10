f4d43f71fc40f140bb85058bb5ac05901ec6b75a
pramudya03 <2300018281@webmail.uad.ac.id>
2024-07-07 19:06:30 +0700

    Add files via upload

---
+++ nilai.php
@@ -0,0 +1,10 @@
+<?php
+$nilai = 50;
+if ($nilai >= 60) {
+    echo "Nilai Anda $nilai, Anda LULUS\n";
+    echo "Selamat! Terus pertahankan prestasimu.";
+} else {
+    echo "Nilai Anda $nilai, Anda GAGAL\n";
+    echo "Jangan putus asa, terus berusaha dan belajar lebih giat.";
+}
+?>
